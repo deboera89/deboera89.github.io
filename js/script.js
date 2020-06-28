1f1e6696b22bbbc048c1fd6bdbdde21f10b14e78
deboera89 <ashleedeboer@gmail.com>
2020-06-28 21:35:37 +0930

    js animate

--- js/script.js
+++ js/script.js
@@ -15,11 +15,31 @@ function showDiv(id, buttonID) {
 	var ele = document.getElementById(id);
 	var button = document.getElementById(buttonID).setAttribute('class', 'content-button content-selected');
 	ele.setAttribute('class', 'info-box');
+	var contentDiv = ele.children;
+	contentDiv[0].style.left = '-742px';
+	animateDivEntry(contentDiv[0]);
 	
 }
 
+function animateDivEntry(divToAnimate) {
+  var pos = -742;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos >= 0) {
+		divToAnimate.style.overflowY = "auto";
+		clearInterval(id);
+    } else {
+      pos+=7; 
+      divToAnimate.style.left = pos + "px"; 
+    }
+  }
+}
+
+
 function hideDiv(id) {
 	document.getElementById(id).setAttribute('class', 'info-box display-none');
+	var contentDiv = document.getElementById(id).children;
+	contentDiv[0].style.overflowY = "hidden";
 }
 
 function deselectButton(id) {
